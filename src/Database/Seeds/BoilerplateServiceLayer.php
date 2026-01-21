ba4676c34464a4dd558eb9cee74fcdc3da6d0dfd
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2026-01-20 17:08:36 -0700

    Corrections in  permission install

--- src/Database/Seeds/BoilerplateServiceLayer.php
+++ src/Database/Seeds/BoilerplateServiceLayer.php
@@ -38,10 +38,15 @@ class BoilerplateServiceLayer extends Seeder {
 
         // Permission
         $this->authorize->createPermission('servicelayer-permission', 'Permissions for service layer configuration');
+        $this->authorize->createPermission('user_sap_link-permission', 'Permiso para la lista de user_sap_link');
+        $this->authorize->createPermission('reqauth-permission', 'Permisos para autorizar requisiciones del SAP');
+        $this->authorize->createPermission('poauth-permission', 'Permiso Para Autorizar');
 
         // Assign Permission to user
         $this->authorize->addPermissionToUser('servicelayer-permission', 1);
-
+        $this->authorize->addPermissionToUser('user_sap_link-permission', 1);
+        $this->authorize->addPermissionToUser('reqauth-permission', 1);
+        $this->authorize->addPermissionToUser('poauth-permission', 1);
     }
 
     public function down() {
